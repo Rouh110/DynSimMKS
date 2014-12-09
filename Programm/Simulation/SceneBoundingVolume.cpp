ee42ebaa644b70c37a7eacb2bf436205a5771687
Jens Sperling <sperling@dirty-bits.de>
2014-12-09 11:13:30 +0100

    weitere scene

---
+++ Programm/Simulation/SceneBoundingVolume.cpp
@@ -0,0 +1,43 @@
+#include "SceneBoundingVolume.h"
+#include "Sphere.h"
+#include "Gravity.h"
+#include "Simulation/Spring.h"
+#include "Common/Config.h"
+#include "Visualization/MiniGL.h"
+#include "GL/glut.h"
+#include "TimeManager.h"
+#include "Common/StringTools.h"
+#include "Common/timing.h"
+
+SceneBoundingVolume::SceneBoundingVolume()
+{
+}
+
+
+SceneBoundingVolume::~SceneBoundingVolume()
+{
+}
+void SceneBoundingVolume::initializeScene(){
+	timer = 0;
+	Gravity::create();
+	bouncingball = &Sphere::create(0.4);
+	bouncingball->setPosition(Eigen::Vector3d(0, 3, 0));
+	bouncingball->setMass(1);
+	boundBouncingBall = new BoundingVolume(Eigen::Vector3d(0,3,0), 0.4);	
+}
+void SceneBoundingVolume::update(Real currentTime)
+{
+	int timeFactor = 5;
+	Real impulseStrength = 0.1;
+	
+	// set Timer
+	if (timer < (int)(currentTime / timeFactor))
+	{
+		timer++;
+	}
+	boundBouncingBall->m = bouncingball->getPosition();
+	if (boundBouncingBall->collisionTestYAxis()){
+		std::cout << "ConteactNormal axis " << boundBouncingBall->contactNormal << " \n" << std::flush;
+		boundBouncingBall->collisionCalcYAxis();
+	}
+}
