31ff212a4f65a17aad15f1ac99451549933dc1d0
Rouh <janulrichschmitt@hotmail.de>
2014-12-10 20:42:16 +0100

    Test Scene erweitert ruckelt starck und sollte vielleicht mit einer kleineren baum tiefe ausgeführt werden. Kompiliert

--- Programm/Simulation/SceneCollisionTest01.cpp
+++ Programm/Simulation/SceneCollisionTest01.cpp
@@ -1,7 +1,5 @@
 #include "SceneCollisionTest01.h"
 
-#include "Cube.h"
-#include "Sphere.h"
 
 SceneCollisionTest01::SceneCollisionTest01()
 {
@@ -16,14 +14,73 @@ SceneCollisionTest01::~SceneCollisionTest01()
 
 void SceneCollisionTest01::initializeScene()
 {
+	timer = 0;
+	next = false;
+
+	Real y = 1;
+	Real y2 = 3;
+	Real x1 = -2.5;
+	Real x2 = -1;
+	Real x3 = 0.5;
+	Real x4 = 3;
+
+	// base line
+	cube01 = &Cube::create();
+	cube01->setPosition(Vector3d(x1,y,0));
+	cube02 = &Cube::create();
+	cube02->setPosition(Vector3d(x2,y,0));
+	sphere01 = &Sphere::create(0.5);
+	sphere01->setPosition(Vector3d(x3, y, 0));
+
+
+	cube04 = &Cube::create();
+	cube04->setPosition(Vector3d(x4, y, 0));
+	cube05 = &Cube::create();
+	cube05->setPosition(Vector3d(x4, y2, 0));
+	// fall line
+	cube03 = &Cube::create();
+	cube03->setPosition(Vector3d(x1, y2, 0));
+	sphere02 = &Sphere::create(0.5);
+	sphere02->setPosition(Vector3d(x2, y2, 0));
+	sphere03 = &Sphere::create(0.5);
+	sphere03->setPosition(Vector3d(x3, y2, 0));
 	
-	Cube &cube01 = Cube::create();
-	Cube &cube02 = Cube::create();
-	cube02.setPosition(Eigen::Vector3d(1+(0.3),0,0));
 
-	Cube &cube03 = Cube::create();
-	cube03.setPosition(Eigen::Vector3d(0, 2, 0));
-	Sphere &sphere01 = Sphere::create(0.5);
-	sphere01.setPosition(Eigen::Vector3d(0.9, 2, 0));
+}
 
+void SceneCollisionTest01::update(Real currentTime)
+{
+	int timeFactor = 5;
+	Real impulseStrength = 0.1;
+
+	// set Timer
+	if (timer < (int)(currentTime / timeFactor))
+	{
+		timer++;
+		next = true;
+	}
+
+	if (next)
+	{
+		switch (timer)
+		{
+		case 1:
+			cube03->addRasImpuls(Vector3d(0, -impulseStrength, 0), Vector3d(0, 0, 0));
+			break;
+		case 2:
+			sphere02->addRasImpuls(Vector3d(0, -impulseStrength, 0), Vector3d(0, 0, 0));
+			break;
+		case 3:
+			sphere03->addRasImpuls(Vector3d(0, -impulseStrength, 0), Vector3d(0, 0, 0));
+			break;
+		case 4:
+			cube05->addRasImpuls(Vector3d(0, -impulseStrength, 0), Vector3d(1, 0, 1));
+			break;
+		case 10:
+			// reset();
+			break;
+		}
+
+		next = false;
+	}
 }
--- Programm/Simulation/SceneCollisionTest01.h
+++ Programm/Simulation/SceneCollisionTest01.h
@@ -1,11 +1,29 @@
 #pragma once
 #include "IScene.h"
+#include "Cube.h"
+#include "Sphere.h"
+using namespace Eigen;
 class SceneCollisionTest01 : public IScene
 {
+protected:
+	Cube * cube01;
+	Cube * cube02;
+	Cube * cube03;
+	Cube * cube04;
+	Cube * cube05;
+
+	Sphere * sphere01;
+	Sphere * sphere02;
+	Sphere * sphere03;
+
+	int timer;
+	bool next;
+	
 public:
 	SceneCollisionTest01();
 	~SceneCollisionTest01();
 
 	void initializeScene();
+	void update(Real currentTime);
 };
 
